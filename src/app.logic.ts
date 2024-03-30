4de307d3cc5ae682c20ab2cbfe76899fdcec3b78
Daniel Restrepo <jdrestrepo@unitecnica.net>
2024-03-30 18:52:11 -0500

    Show argument values by console

---
+++ src/app.logic.ts
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const base = 5;
+let fiveTable:string = `
+=====================     
+     Tabla del ${ 5 }
+======================\n`;
+
+for( let i = 1; i < 11; i++ ) {
+    fiveTable += `5 x ${ i } = ${ base * i }\n`;
+}
+
+const outputsPath = 'outputs';
+
+fs.mkdirSync(outputsPath, {recursive: true});
+fs.writeFileSync(`${ outputsPath }/tabla-${ base }`, fiveTable);
+console.log('File created!!')
+
+
+
+
+
--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,2 @@
-const fs = require('fs');
 
-const base = 5;
-let fiveTable:string = `
-=====================     
-     Tabla del ${ 5 }
-======================\n`;
-
-for( let i = 1; i < 11; i++ ) {
-    fiveTable += `5 x ${ i } = ${ base * i }\n`;
-}
-
-const outputsPath = 'outputs';
-
-
-fs.mkdirSync(outputsPath, {recursive: true});
-fs.writeFileSync(`${ outputsPath }/tabla-${ base }`, fiveTable);
-console.log('File created!!')
-
-
-
-
-
+console.log(process.argv)

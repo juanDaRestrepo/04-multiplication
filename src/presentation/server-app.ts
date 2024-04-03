811ac887b1a0e82bbe1d0c76684300a466d17cac
Daniel Restrepo <jdrestrepo@unitecnica.net>
2024-04-02 19:29:37 -0500

    Create SaveFile use case

--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -1,4 +1,5 @@
 import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use-case";
 
 interface RunOptions {
     base: number;
@@ -13,9 +14,17 @@ export class ServerApp {
         console.log('Server running...');
 
         const table = new CreateTable().execute({ base, limit });
+        const wasCreated = new SaveFile()
+            .execute({
+                fileContent: table,
+                fileDestination: `outputs/table-${ base }`
+            });
 
-        if( showTable ) {
-            console.log(table)
-        }
+        if ( showTable ) console.log(table);
+
+        ( wasCreated )
+            ? console.log('File created!')
+            : console.error('File not created!!');
+        
     }
 }

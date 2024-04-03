d8d74eef73ed9055a0e4f96ad58d0dff6c83adfb
Daniel Restrepo <jdrestrepo@unitecnica.net>
2024-04-02 20:31:53 -0500

    Create logic to send destination and fileName

--- src/config/plugins/args.plugin.ts
+++ src/config/plugins/args.plugin.ts
@@ -20,6 +20,18 @@ export const yarg = yargs(hideBin(process.argv))
         default: false,
         describe: 'Show multiplication table'
     })
+    .option('n', {
+        alias: 'name',
+        type: 'string',
+        default: 'table',
+        describe: 'File name'
+    })
+    .option('d', {
+        alias: 'destination',
+        type: 'string',
+        default: './outputs',
+        describe: 'File destination'
+    })
     .check(( argv ) => {
         if ( argv.b < 1 ) throw 'Error: base debe ser mayor a 0';
         return true;
--- src/presentation/server-app.ts
+++ src/presentation/server-app.ts
@@ -2,22 +2,25 @@ import { CreateTable } from "../domain/use-cases/create-table.use-case";
 import { SaveFile } from "../domain/use-cases/save-file.use-case";
 
 interface RunOptions {
-    base: number;
-    limit: number;
-    showTable: boolean;
+    base       : number;
+    limit      : number;
+    showTable  : boolean;
+    fileName   : string;
+    destination: string;
 }
 
 
 export class ServerApp {
 
-    static run({base, limit, showTable}: RunOptions) {
+    static run({base, limit, showTable, fileName, destination}: RunOptions) {
         console.log('Server running...');
 
         const table = new CreateTable().execute({ base, limit });
         const wasCreated = new SaveFile()
             .execute({
                 fileContent: table,
-                fileDestination: `outputs/table-${ base }`
+                fileDestination: destination,
+                fileName
             });
 
         if ( showTable ) console.log(table);
